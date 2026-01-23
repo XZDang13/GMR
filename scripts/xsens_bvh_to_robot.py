09960ea3b86bf5a7d1fb5ad7b7374b5cdce2df61
YanjieZe <zeyanjie@sjtu.edu.cn>
2026-01-23 10:23:38 -0800

    [update default scale of bvh xsens]

--- scripts/xsens_bvh_to_robot.py
+++ scripts/xsens_bvh_to_robot.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--scale",
-        default=1,
+        default=0.01,
         type=float,
         help="The scaling size is determined based on the units used for displacement",
     )
